f845388052fb9c663ce10346a2ce3dc8039c6d9b
夏至夕阳 <honglonglong@users.noreply.github.com>
2021-01-26 23:33:49 +0800

    Update dailyRSS.sh

--- server/dailyRSS.sh
+++ server/dailyRSS.sh
@@ -1,3 +1,3 @@
 rm -f $(dirname $(readlink -f $0))/../temp/*
-echo "running directly:" $(dirname $(readlink -f $0))
+echo "running directory:" $(dirname $(readlink -f $0))
 python $(dirname $(readlink -f $0))/send.py
